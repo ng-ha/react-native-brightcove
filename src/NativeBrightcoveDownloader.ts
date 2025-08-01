ad7dbe6daca2c7d0d6414b1fd07415cc243e833c
ng-ha <ngthanhha.hn@gmail.com>
2025-08-01 15:17:04 +0700

    feat: handle events with payloads

--- src/NativeBrightcoveDownloader.ts
+++ src/NativeBrightcoveDownloader.ts
@@ -1,13 +1,10 @@
 import type { TurboModule } from 'react-native';
 import { TurboModuleRegistry } from 'react-native';
 import type {
+  Double,
   EventEmitter,
   Int32,
 } from 'react-native/Libraries/Types/CodegenTypes';
-
-export type TBrightcoveDownloaderParams = {
-  id: string;
-};
 
 export type TBrightcoveDownloadConfig = {
   accountId: string;
@@ -27,14 +24,58 @@ export type TBrightcoveDownloadedVideo = {
   size?: Int32 | null;
 };
 
+// Download result
+
 export type TBrightcovePauseDownloadResult = {
   status?: Int32 | null;
 };
+
 export type TBrightcoveResumeDownloadResult = {
   status?: Int32 | null;
 };
+
 export type TBrightcoveDeleteVideoResult = {
   result?: boolean | null;
+};
+
+// Download event
+
+export type TBrightcoveDownloadRequestedEvent = {
+  id: string;
+};
+
+export type TBrightcoveDownloadStartedEvent = {
+  id: string;
+  estimatedSize: Int32;
+};
+
+export type TBrightcoveDownloadProgressEvent = {
+  id: string;
+  maxSize: Int32;
+  bytesDownloaded: Int32;
+  progress: Double;
+};
+
+export type TBrightcoveDownloadPausedEvent = {
+  id: string;
+  reason: Int32;
+};
+
+export type TBrightcoveDownloadCompletedEvent = {
+  id: string;
+};
+
+export type TBrightcoveDownloadCanceledEvent = {
+  id: string;
+};
+
+export type TBrightcoveDownloadDeletedEvent = {
+  id: string;
+};
+
+export type TBrightcoveDownloadFailedEvent = {
+  id: string;
+  reason: Int32;
 };
 
 export interface Spec extends TurboModule {
@@ -45,14 +86,14 @@ export interface Spec extends TurboModule {
   pauseVideoDownload(id: string): Promise<TBrightcovePauseDownloadResult>;
   resumeVideoDownload(id: string): Promise<TBrightcoveResumeDownloadResult>;
   deleteVideo(id: string): Promise<TBrightcoveDeleteVideoResult>;
-  readonly onDownloadRequested: EventEmitter<TBrightcoveDownloaderParams>;
-  readonly onDownloadStarted: EventEmitter<TBrightcoveDownloaderParams>;
-  readonly onDownloadProgress: EventEmitter<TBrightcoveDownloaderParams>;
-  readonly onDownloadPaused: EventEmitter<TBrightcoveDownloaderParams>;
-  readonly onDownloadCompleted: EventEmitter<TBrightcoveDownloaderParams>;
-  readonly onDownloadCanceled: EventEmitter<TBrightcoveDownloaderParams>;
-  readonly onDownloadDeleted: EventEmitter<TBrightcoveDownloaderParams>;
-  readonly onDownloadFailed: EventEmitter<TBrightcoveDownloaderParams>;
+  readonly onDownloadRequested: EventEmitter<TBrightcoveDownloadRequestedEvent>;
+  readonly onDownloadStarted: EventEmitter<TBrightcoveDownloadStartedEvent>;
+  readonly onDownloadProgress: EventEmitter<TBrightcoveDownloadProgressEvent>;
+  readonly onDownloadPaused: EventEmitter<TBrightcoveDownloadPausedEvent>;
+  readonly onDownloadCompleted: EventEmitter<TBrightcoveDownloadCompletedEvent>;
+  readonly onDownloadCanceled: EventEmitter<TBrightcoveDownloadCanceledEvent>;
+  readonly onDownloadDeleted: EventEmitter<TBrightcoveDownloadDeletedEvent>;
+  readonly onDownloadFailed: EventEmitter<TBrightcoveDownloadFailedEvent>;
 }
 
 export default TurboModuleRegistry.getEnforcing<Spec>(
