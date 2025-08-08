2f90f8c881746a965bcc187f759e89c9c6bcbdf1
ng-ha <ngthanhha.hn@gmail.com>
2025-08-08 17:30:53 +0700

    .

--- ios/RCTBrightcoveView.mm
+++ ios/RCTBrightcoveView.mm
@@ -1,64 +1,79 @@
 #import "RCTBrightcoveView.h"
 
+#import <BrightcovePlayerSDK/BCOVPUIPlayerView.h>
 #import <react/renderer/components/BrightcoveCodegenSpec/ComponentDescriptors.h>
 #import <react/renderer/components/BrightcoveCodegenSpec/EventEmitters.h>
 #import <react/renderer/components/BrightcoveCodegenSpec/Props.h>
 #import <react/renderer/components/BrightcoveCodegenSpec/RCTComponentViewHelpers.h>
 
 #import "RCTFabricComponentsPlugins.h"
+#import "react_native_brightcove-Swift.h"
 
 using namespace facebook::react;
 
-@interface BrightcoveView () <RCTBrightcoveViewViewProtocol>
+@interface RCTBrightcoveView () <RCTBrightcoveViewViewProtocol, BCOVPUIPlayerViewDelegate>
 
 @end
 
-@implementation BrightcoveView {
-  UIView *_view;
+@implementation RCTBrightcoveView {
+  //  UIView *_view;
+  RCTBrightcoveViewImpl *_brightcoveView;
 }
+
+//- (instancetype)init {
+//  if (self = [super init]) {
+//    _brightcoveView = [[RCTBrightcoveViewImpl alloc] init];
+//    [self addSubview:_brightcoveView];
+//  }
+//  return self;
+//}
 
 - (instancetype)initWithFrame:(CGRect)frame {
   if (self = [super initWithFrame:frame]) {
     static const auto defaultProps = std::make_shared<const BrightcoveViewProps>();
     _props = defaultProps;
-
-    _view = [[UIView alloc] init];
-
-    self.contentView = _view;
+    _brightcoveView = [[RCTBrightcoveViewImpl alloc] init];
+    //    _brightcoveView.delegate = self;
+    self.contentView = _brightcoveView;
   }
-
   return self;
+}
+
+- (void)dealloc {
+  if (_brightcoveView) {
+    //    _brightcoveView.delegate = nil;
+    _brightcoveView = nil;
+  }
+}
+
+//  You can listen to this lifecycle event to pause / resume operations as Fabric components are
+//  kept in memory to be reused later
+//- (void)didMoveToSuperview {
+//  if (self.superview != nil) {
+//    // Manually triggering events that child third-party views/controllers listen to resume
+//    // operations
+//    [_brightcoveView triggerViewWillAppear];
+//  }
+//}
+
+- (void)layoutSubviews {
+  [super layoutSubviews];
+  _brightcoveView.frame = self.bounds;
 }
 
 - (void)updateProps:(Props::Shared const &)props oldProps:(Props::Shared const &)oldProps {
   const auto &oldViewProps = *std::static_pointer_cast<BrightcoveViewProps const>(_props);
   const auto &newViewProps = *std::static_pointer_cast<BrightcoveViewProps const>(props);
 
-//  if (oldViewProps.color != newViewProps.color) {
-//    NSString *colorToConvert = [[NSString alloc] initWithUTF8String:newViewProps.color.c_str()];
-//    [_view setBackgroundColor:[self hexStringToColor:colorToConvert]];
-//  }
+  //  if (oldViewProps.color != newViewProps.color) {
+  //    NSString *colorToConvert = [[NSString alloc] initWithUTF8String:newViewProps.color.c_str()];
+  //    [_view setBackgroundColor:[self hexStringToColor:colorToConvert]];
+  //  }
 
   [super updateProps:props oldProps:oldProps];
 }
 
-Class<RCTComponentViewProtocol> BrightcoveViewCls(void) { return BrightcoveView.class; }
-
-- hexStringToColor:(NSString *)stringToConvert {
-  NSString *noHashString = [stringToConvert stringByReplacingOccurrencesOfString:@"#"
-                                                                      withString:@""];
-  NSScanner *stringScanner = [NSScanner scannerWithString:noHashString];
-
-  unsigned hex;
-  if (![stringScanner scanHexInt:&hex])
-    return nil;
-  int r = (hex >> 16) & 0xFF;
-  int g = (hex >> 8) & 0xFF;
-  int b = (hex) & 0xFF;
-
-  return [UIColor colorWithRed:r / 255.0f green:g / 255.0f blue:b / 255.0f alpha:1.0f];
-}
-
+//Class<RCTComponentViewProtocol> RCTBrightcoveViewCls(void) { return RCTBrightcoveView.class; }
 
 - (void)pause {
 }
@@ -76,6 +91,16 @@ Class<RCTComponentViewProtocol> BrightcoveViewCls(void) { return BrightcoveView.
 }
 
 - (void)toggleInViewPort:(BOOL)inViewPort {
+}
+
+// Event emitter convenience method
+- (const BrightcoveViewEventEmitter &)eventEmitter {
+  return static_cast<const BrightcoveViewEventEmitter &>(*_eventEmitter);
+}
+
+// Override the default handleCommand with RCTBrightcoveViewHandleCommand helper
+- (void)handleCommand:(const NSString *)commandName args:(const NSArray *)args {
+  RCTBrightcoveViewHandleCommand(self, commandName, args);
 }
 
 + (ComponentDescriptorProvider)componentDescriptorProvider {
