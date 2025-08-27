dce9b539cfd9883838d155da23e755d5425333a8
ng-ha <ngthanhha.hn@gmail.com>
2025-08-27 15:46:42 +0700

    chore(example): add screen navigation to test component lifecycle

---
+++ example/src/configs/index.ts
@@ -0,0 +1,30 @@
+// export const accountId = '5434391461001';
+// export const policyKey =
+//   'BCpkADawqM0T8lW3nMChuAbrcunBBHmh4YkNl5e6ZrKQwPiK_Y83RAOF4DP5tyBF_ONBVgrEjqW6fbV0nKRuHvjRU3E8jdT9WMTOXfJODoPML6NUDCYTwTHxtNlr5YdyGYaCPLhMUZ3Xu61L';
+// export const videoId = '6140448705001';
+
+// export const accountId = '5420904993001';
+// export const policyKey =
+//   'BCpkADawqM1RJu5c_I13hBUAi4c8QNWO5QN2yrd_OgDjTCVsbILeGDxbYy6xhZESTFi68MiSUHzMbQbuLV3q-gvZkJFpym1qYbEwogOqKCXK622KNLPF92tX8AY9a1cVVYCgxSPN12pPAuIM';
+// export const playlistReferenceId = 'demo_odrm_widevine_dash';
+// export const videoIds = [
+//   '5421538222001',
+//   '5421543459001',
+//   '5421546903001',
+//   '5421531913001',
+//   '5421538244001',
+// ];
+
+export const accountId = '5434391461001';
+export const policyKey =
+  'BCpkADawqM0T8lW3nMChuAbrcunBBHmh4YkNl5e6ZrKQwPiK_Y83RAOF4DP5tyBF_ONBVgrEjqW6fbV0nKRuHvjRU3E8jdT9WMTOXfJODoPML6NUDCYTwTHxtNlr5YdyGYaCPLhMUZ3Xu61L';
+export const videoIds = [
+  '5702141808001',
+  '5702143019001',
+  '5702147184001',
+  '5702149062001',
+  '5702143016001',
+  '5702148954001',
+  '1732548841120406830',
+];
+export const videoId = videoIds[0] as string;
